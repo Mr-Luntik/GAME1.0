73290033bfe560024cdbbf2dd6395036cfa558a0
Andrey <barahtenkoa535@gmail.com>
2024-07-29 15:50:17 +0800

    Движение игрока работает только если мы двигаем мышью(так не должно быть).

--- GAME1.0/main.cpp
+++ GAME1.0/main.cpp
@@ -3,23 +3,28 @@
 #include "view.h"
 #include <iostream>
 #include <sstream>
+#include "mission.h"
 using namespace sf;
+using namespace std;
 
 class Player
 {
-private: float x, y = 0;
+
 public:
-	float w, h, dx, dy, speed = 0;
+	float w, h, dx, dy, speed = 0, x, y = 0;
 	int dir, playerScore, health;
-	bool life;
+	bool life, isMove, isSelect;
 	String File;
 	Image image;
 	Texture texture;
 	Sprite sprite;
+
 	Player(String F, int X, int Y, float W, float H)
 	{
-		dir = 0; speed = 0; playerScore = 0, health = 100;
+		dir = 0; speed = 0; playerScore = 0, health = 100, dx = 0, dy = 0;
 		life = true;
+		isMove = false;
+		isSelect = false;
 		File = F; // Имя файла + расширение
 		w = W; h = H; //Высота и ширина
 		image.loadFromFile("images/" + File);
@@ -110,13 +115,13 @@ public:
 int main()
 {
 
-	RenderWindow window(sf::VideoMode(640, 480), "Lesson 1. kychka-pc.ru");
+	RenderWindow window(sf::VideoMode(1366, 768), "Lesson 1. kychka-pc.ru"/*, Style::Fullscreen*/);
 	view.reset(FloatRect(0, 0, 640, 480));
 
 	Font font;//шрифт 
 	font.loadFromFile("CyrilicOld.ttf");//передаем нашему шрифту файл шрифта
 	Text text("", font, 20);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
-	text.setFillColor (Color::Red);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
+	text.setFillColor (Color::Black);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
 	text.setStyle(sf::Text::Bold | sf::Text::Underlined);
 
 	Image heroimog;
@@ -128,13 +133,33 @@ int main()
 	map.loadFromImage(map_image);
 	Sprite s_map;
 	s_map.setTexture(map);
+
+	Image quest_image;
+	quest_image.loadFromFile("images/missionbg.jpg");
+	quest_image.createMaskFromColor(Color(0, 0, 0));
+	Texture quest_texture;
+	quest_texture.loadFromImage(quest_image);
+	Sprite s_quest;
+	s_quest.setTexture(quest_texture);
+	s_quest.setTextureRect(IntRect(0, 0, 340, 510));  //приведение типов, размеры картинки исходные
+	s_quest.setScale(0.6f, 0.6f);//чуть уменьшили картинку, => размер стал меньше
+	
 	Player p("hero.png", 250, 250, 96.0, 96.0);
+
+	bool showMissionText = true;
 
 	float CurrentFrame = 0;
 	Clock clock;
 	Clock gameTimeClock;
 	int gameTime = 0;
+	bool isMove = false;
+	float dX = 0;
+	float dY = 0;
+	int tempX = 0;
+	int tempY = 0;
+	float distance = 0;
 
+	int createObjectForMapTimer = 0;
 
 	while (window.isOpen())
 	{
@@ -143,13 +168,80 @@ int main()
 		clock.restart();
 		time = time / 800;
 
+		Vector2i pixelPos = Mouse::getPosition(window);
+		Vector2f pos = window.mapPixelToCoords(pixelPos);
+
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == Event::MouseButtonPressed)//если нажата клавиша мыши
+				if (event.key.code == Mouse::Left)//а именно левая
+				{
+					if (p.sprite.getGlobalBounds().contains(pos.x, pos.y))//и при этом координата курсора попадает в спрайт
+					{
+						p.sprite.setColor(Color::Green);
+						p.isSelect = true;
+					}
+				}
+
+			if (p.isSelect)
+				if (event.type == Event::MouseButtonPressed)
+					if (event.key.code == Mouse::Right)
+					{
+						p.isMove = true;
+						p.isSelect = false;
+						p.sprite.setColor(Color::White);
+						tempX = pos.x;
+						tempY = pos.y;
+					}
+
+			if (p.isMove) {
+				distance = sqrt((tempX - p.x) * (tempX - p.x) + (tempY - p.y) * (tempY - p.y));//считаем дистанцию (длину от точки А до точки Б). формула длины вектора
+
+				if (distance > 2) {//этим условием убираем дергание во время конечной позиции спрайта
+
+					p.x += 0.1 * time * (tempX - p.x) / distance;//идем по иксу с помощью вектора нормали
+					p.y += 0.1 * time * (tempY - p.y) / distance;//идем по игреку так же
+				}
+				else { p.isMove = false; std::cout << "priehali\n"; }//говорим что уже никуда не идем и выводим веселое сообщение в консоль
+			}
+	
+
+			//if (event.type == Event::KeyPressed)//событие нажатия клавиши
+			//	if ((event.key.code == Keyboard::Tab)) {//если клавиша ТАБ
+
+
+			//		switch (showMissionText) {//переключатель, реагирующий на логическую переменную showMissionText
+
+			//		case true: {
+			//			std::ostringstream playerHealthString;//строка здоровья игрока
+			//			playerHealthString << p.health; //заносим в строку здоровье 
+			//			std::ostringstream task;//строка текста миссии
+			//			task << getTextMission(getCurrentMission(p.getPlayercoordinateX()));//вызывается функция getTextMission (она возвращает текст миссии), которая принимает в качестве аргумента функцию getCurrentMission(возвращающую номер миссии), а уже эта ф-ция принимает в качестве аргумента функцию p.getplayercoordinateX() (эта ф-ция возвращает Икс координату игрока)
+			//			text.setString("Здоровье: " + playerHealthString.str() + "\n" + task.str());//задаем
+			//			text.setPosition(view.getCenter().x + 125, view.getCenter().y - 130);//позиция всего этого текстового блока
+			//			s_quest.setPosition(view.getCenter().x + 115, view.getCenter().y - 130);//позиция фона для блока
+			//			showMissionText = false;//эта строка позволяет убрать все что мы вывели на экране
+			//			break;//выходим , чтобы не выполнить условие "false" (которое ниже)
+			//		}
+			//		case false: {
+			//			text.setString("");//если не нажата клавиша таб, то весь этот текст пустой
+			//			showMissionText = true;// а эта строка позволяет снова нажать клавишу таб и получить вывод на экран
+			//			break;
+			//		}
+			//		}
+			//	}
 		}
-		
+
+		if ((Keyboard::isKeyPressed(Keyboard::Escape)))
+		{
+			window.close();
+		}
+
+
 		if (p.life)
 		{
 			if ((Keyboard::isKeyPressed(Keyboard::Left)))
@@ -187,35 +279,54 @@ int main()
 				p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
 
 			}
+			getPlayercoordinateForView(p.getPlayercoordinateX(), p.getPlayercoordinateY());
 		}
+
 		
-		getPlayercoordinateForView(p.getPlayercoordinateX(), p.getPlayercoordinateY());
+		sf::Vector2i localPosition = Mouse::getPosition(window);
+		
+		if (localPosition.x < 3) { view.move(-0.2 * time, 0); }
+		if (localPosition.x > window.getSize().x - 3) { view.move(0.2 * time, 0); }
+		if (localPosition.y > window.getSize().y - 3) { view.move(0, 0.2 * time); }
+		if (localPosition.y < 3) { view.move(0, -0.2 * time); }
+
 
 		p.update(time);
 		viewmap(time);
 		window.setView(view);
 		window.clear();
 		//Рисуем карту
-		for (int i = 0; i < HEIGHT_MAP; i++)
-			for (int j = 0; j < WIDTH_MAP; j++)
-			{
-				if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); //если встретили символ пробел, то рисуем 1й квадратик
-				if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));//если встретили символ s, то рисуем 2й квадратик
-				if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если встретили символ 0, то рисуем 3й квадратик
-				if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32)); //Добавляем уветок
-				if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32)); //Добавляем Сердечко
-
-
-				s_map.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
-
-				window.draw(s_map);//рисуем квадратики на экран
-			}
-		std::ostringstream playerHealtString, gameTimeString;
-		
-		playerHealtString << p.health; gameTimeString << gameTime;		//формируем строку
-		text.setString("Здоровье: " + playerHealtString.str() + "\nВремя игры: " + gameTimeString.str());
-		text.setPosition(view.getCenter().x - 165, view.getCenter().y - 200);
-		window.draw(text);
+		if ((getCurrentMission(p.getPlayercoordinateX())) == 0) { //Если текущая миссия 0, то рисуем карту вот так
+			for (int i = 0; i < HEIGHT_MAP; i++)
+				for (int j = 0; j < WIDTH_MAP; j++)
+				{
+					if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32));
+					if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));
+					if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
+					if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));
+					if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));
+					s_map.setPosition(j * 32, i * 32);
+					window.draw(s_map);
+				}
+		}
+		if ((getCurrentMission(p.getPlayercoordinateX())) >= 1) { //Если текущая миссия 1, то рисуем карту вот так
+			for (int i = 0; i < HEIGHT_MAP; i++)
+				for (int j = 0; j < WIDTH_MAP; j++)
+				{
+					if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(64, 0, 32, 32));//для примера поменял местами вывод спрайта для этого символа и..
+					if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));
+					if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(0, 0, 32, 32));//и для вот этого. логически-игровой смысл их остался таким же
+					if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));
+					if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));
+					s_map.setPosition(j * 32, i * 32);
+					window.draw(s_map);
+				}
+		}
+		if (!showMissionText) {
+			text.setPosition(view.getCenter().x + 125, view.getCenter().y - 130);//позиция всего этого текстового блока
+			s_quest.setPosition(view.getCenter().x + 115, view.getCenter().y - 130);//позиция фона для блока			
+			window.draw(s_quest); window.draw(text); //рисуем спрайт свитка (фон для текста миссии). а затем и текст. все это завязано на логическую переменную, которая меняет свое состояние от нажатия клавиши ТАБ
+		}
 
 		window.draw(p.sprite);
 		
--- GAME1.0/map.h
+++ GAME1.0/map.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
+using namespace std;
+
 const int HEIGHT_MAP = 25;
 const int WIDTH_MAP = 40;
 
@@ -31,3 +34,23 @@ sf::String TileMap[HEIGHT_MAP] = {
 	"0                                      0",
 	"0000000000000000000000000000000000000000",
 };
+
+void randomMapGenerate()
+{
+	int randomElementX = 0;
+	int randomElementY = 0;
+	srand(time(0));
+	int counterStone = 1;
+
+	while (counterStone > 0)
+	{
+		randomElementX = 1 + rand() % (WIDTH_MAP - 1);
+		randomElementY = 1 + rand() % (HEIGHT_MAP - 1);
+		if (TileMap[randomElementY][randomElementX] == ' ')
+		{
+			TileMap[randomElementY][randomElementX] = 's';
+			cout << "coordinate of stone X: " << randomElementX << "\n" << "coordinate of stone Y: " << randomElementY << "\n\n";
+			counterStone--;
+		}
+	}
+}
